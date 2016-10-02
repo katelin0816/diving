79f06f87b25acd72a28b2376c7608422ef7551f5
Kate Lin <cici21818@gmail.com>
2016-10-02 18:33:04 +0800

    0.0.1

---
+++ assets/js/customize.js
@@ -0,0 +1,58 @@
+/*-----------------------------------------------------------------------------------
+/* Customize JS
+-----------------------------------------------------------------------------------*/
+$(document).ready(function() {
+
+
+    //fullpage js
+    $('#fullpage').fullpage({
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
+        sectionsColor: ['#FFFFFF', '#FFFFFF', '#FFFFFF'],
+        navigation: true,
+        scrollingSpeed: 1000,
+        easing: 'easeInOutCubic',
+        css3: false,
+        scrollOverflow: true,
+        scrollOverflowOptions: {
+            click: true
+        },
+        afterLoad: function(anchorLink, index) {
+
+            $(".iScrollIndicator").css({
+                "display": "none"
+            });
+
+            //section 2
+            if (index == 1) {
+                $('#section1').find('h1').delay(800).css({
+                    opacity: 1,
+                    transition: '1.0s'
+                });
+            }
+
+            //section 2
+            if (index == 2) {
+
+                $('#section2').find('h1').delay(800).css({
+                    opacity: 1,
+                    transition: '1.0s'
+                });
+                $('header').css({
+                    backgroundColor: 'rgba(0,0,0,0)',
+                    transition: '1.0s'
+                });
+            }
+
+            //section 3
+            if (index == 3) {
+                $('header').css({
+                    backgroundColor: 'rgba(0,0,0,0.8)',
+                    transition: '1.0s'
+                });
+            }
+        }
+
+    });
+
+
+});
