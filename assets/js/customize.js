e14c82fc18cc0b16153a243261fd9030ef34450f
Kate Lin <cici21818@gmail.com>
2016-10-05 14:07:03 +0800

    fx

--- assets/js/customize.js
+++ assets/js/customize.js
@@ -10,6 +10,42 @@ $(document).ready(function() {
         verticalCentered: false,
         css3: false,
 
+        afterLoad: function(anchorLink, index) {
+
+            $(".iScrollIndicator").css({
+                "display": "none"
+            });
+
+            //section 2
+            /*if (index == 1) {
+                $('#section1').find('h1').delay(800).css({
+                    opacity: 1,
+                    transition: '1.0s'
+                });
+            }
+
+            //section 2
+            if (index == 2) {
+
+                $('#section2').find('h1').delay(800).css({
+                    opacity: 1,
+                    transition: '1.0s'
+                });
+                $('header').css({
+                    backgroundColor: 'rgba(0,0,0,0)',
+                    transition: '1.0s'
+                });
+            }
+
+            //section 3
+            if (index == 3) {
+                $('header').css({
+                    backgroundColor: 'rgba(0,0,0,0.8)',
+                    transition: '1.0s'
+                });
+            }*/
+        }
+
     });
 
 
