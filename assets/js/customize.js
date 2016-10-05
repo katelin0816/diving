a3c65eb4caf567b882284530307a6cee937a8d95
Kate Lin <cici21818@gmail.com>
2016-10-05 10:31:05 +0800

    fixed bg

--- assets/js/customize.js
+++ assets/js/customize.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
         sectionsColor: ['#FFFFFF', '#FFFFFF', '#FFFFFF'],
         verticalCentered: false,
+        css3: false,
         navigation: true,
         scrollingSpeed: 1000,
         scrollOverflow: true,
